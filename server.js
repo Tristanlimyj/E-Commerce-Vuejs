207edd11351011378c932dee4f4a82b06e3edc36
tristanlimyj <tristanlimyj@gmail.com>
2020-08-10 12:16:14 +0800

    remove ssl

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express') 
-const secure = require('ssl-express-www')
-
 const app = express()
-app.use(secure)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/dist"));
 app.get(/.*/, function(req, res) {
