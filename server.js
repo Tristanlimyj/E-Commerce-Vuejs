848b2ff637af65738da0d95c083a3674efc6bb9f
tristanlimyj <tristanlimyj@gmail.com>
2020-08-10 12:05:22 +0800

    try

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const secure = require('ssl-express-www')
 const app = express()
 app.use(secure)
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/dist"));
 app.get(/.*/, function(req, res) {
