f5e054bcfd25b8c039ce864dbe30d1b3ddfd447c
tristanlimyj <tristanlimyj@gmail.com>
2020-09-10 04:13:19 +0800

    heroku url change

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,8 @@ const store = new Vuex.Store({
   state: {
     status: '',
     currentUser: {},
-    apiurl: 'https://2efeded5bd1b.ngrok.io',
-    // apiurl: 'https://api.thealchemistalcohol.com',
+    // apiurl: 'https://2efeded5bd1b.ngrok.io',
+    apiurl: 'https://api.thealchemistalcohol.com',
     baseurl: 'https://www.thealchemistalcohol.com',
   },
   getters: {
