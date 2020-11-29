e7579f363f7bfc0ee4d37f8b3d6eda6b65dfce7e
tristanlimyj <tristanlimyj@gmail.com>
2020-11-29 18:53:50 +0800

    adding https

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express()
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/dist"));
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(express.static(__dirname + "/dist"));
 
 app.get(/.*/, function(req, res) {
     res.sendfile(__dirname + "/dist/index.html")
