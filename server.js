015bf5d744216ab84b0f71be9102f385f25a37e5
tristanlimyj <tristanlimyj@gmail.com>
2021-01-21 19:13:33 +0800

    remove force https fromj the express file as it will be enforced by the server

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express')
 const http = require('http')
-const enforce = require('express-sslify');
-
 const app = express()
 
 const port = 3000;
-
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(express.static(__dirname + "/dist"));
 
 app.get(/.*/, function(req, res) {
